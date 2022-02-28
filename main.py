5c04e0fbdafb92de23cf7794fd953493e96d350b
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-28 11:15:40 +0530

    printed id, detect.score, detect.location_data.relative_bounding_box, draw default rectangle using drawing_utils, draw rectangle by manually

--- main.py
+++ main.py
@@ -13,7 +13,24 @@ while True:
     success, img = cap.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = face_detect.process(img_rgb)
-    print(result.detections)
+    # print(result.detections)
+    if result.detections:
+        for id, detect in enumerate(result.detections):
+            # mp_draw.draw_detection(img, detect)
+            # print(id)
+            # print(detect.score)
+            # print(id, detect)
+            # print(detect.location_data.relative_bounding_box)
+            bound_box_class = detect.location_data.relative_bounding_box
+            img_height, img_width, img_channel = img.shape
+            bound_box = int(bound_box_class.xmin * img_width), \
+                        int(bound_box_class.ymin * img_height), \
+                        int(bound_box_class.width * img_width), \
+                        int(bound_box_class.height * img_height)
+            cv2.rectangle(
+                img, bound_box,
+                (255, 0, 255), 2
+            )
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
